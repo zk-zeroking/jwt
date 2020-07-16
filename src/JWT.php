6f22fec4e922d32c1fe7aa36eeb8b2d59b8b3fcf
zengzhonggang <zengzhonggang@kf5.com>
2020-07-17 00:33:20 +0800

    base

--- src/JWT.php
+++ src/JWT.php
@@ -1,8 +1,36 @@
 <?php
+declare(strict_types = 1);
 namespace ZeroKing;
 
 class JWT
 {
+    private $header = ['typ' => 'JWT'];
+    private $payload = [];
+    private $key = '';
+    public function getToken():string {
+        return $this->base64UrlEncode($this->header).'.'.$this->base64UrlEncode($this->payload).'.'.$this->signature();
+    }
+    public function setHeader($key,$value):JWT {
+        $this->header[$key] = $value;
+        return $this;
+    }
+    public function setPayload($key,$value):JWT {
+        $this->payload[$key] = $value;
+        return $this;
+    }
+    public function setKey($key):JWT {
+        $this->key = $key;
+        return $this;
+    }
+    private function base64UrlEncode($value):string {
 
+    }
 
+    private function base64Decode($value):string {
+
+    }
+
+    private function signature(){
+        return hash_hmac('sha256',$this->base64UrlEncode($this->header).'.'.$this->base64UrlEncode($this->payload),$this->key);
+    }
 }
